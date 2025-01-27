4bfb853f9b8fd5cd7095cef641e0a8bb0b64bfe9
Winner Isac KODJO <kodjowinnerisac@gmail.com>
2025-01-27 14:39:51 +0100

    Creation d architecture et debut du projet

--- backend/middleware/roleMiddleware.js
+++ backend/middleware/roleMiddleware.js
@@ -1,7 +1,10 @@
-const roleMiddleware = (roles) => (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-        return res.status(403).send('Access Forbidden');
-    }
-    next();
+const roleMiddleware = (role) => {
+    return (req, res, next) => {
+        if (req.user.role !== role) {
+            return res.status(403).json({ message: 'Accès interdit. Rôle insuffisant.' });
+        }
+        next();
+    };
 };
+
 module.exports = roleMiddleware;
